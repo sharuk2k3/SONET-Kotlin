fa68ceff3338c01807fd61c05e33c6b980bb7de0
sharuk2k3 <sharuksharuk46@gmail.com>
2021-06-18 21:03:26 +0530

    Quiz App Day-3

--- app/src/main/java/com/example/myapplication/SonetQuiz.kt
+++ app/src/main/java/com/example/myapplication/SonetQuiz.kt
@@ -12,17 +12,19 @@ import android.widget.Toast
 class SonetQuiz : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sonet_project1)
+        setContentView(R.layout.activity_sonet_quiz)
 
         var q_btn = findViewById<Button>(R.id.quiz_btn)
         var q_name = findViewById<EditText>(R.id.quiz_name)
 
         q_btn.setOnClickListener(View.OnClickListener {
-            Log.i("------->", "Hello")
-            if (q_name.text.toString().isEmpty()) {
-                Toast.makeText(this@SonetQuiz, "Please enter name", Toast.LENGTH_LONG).show()
-            } else {
-                var next: Intent = Intent(this, SonetProject1()::class.java)
+            Log.i("------->","Hello")
+            if(q_name.text.toString().isEmpty()){
+                Toast.makeText(this@SonetQuiz,"Please enter name",Toast.LENGTH_LONG).show()
+            }
+            else{
+                var next:Intent = Intent(this,SonetProject1()::class.java)
+                next.putExtra("name",q_name.text.toString())
                 startActivity(next)
                 finish()
             }
