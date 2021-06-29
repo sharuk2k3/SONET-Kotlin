4d10eef8f2eb2738eb5aac26a1459ee6385558e1
sharuk2k3 <sharuksharuk46@gmail.com>
2021-06-29 20:26:48 +0530

    Room DataBase Day-1

---
+++ app/src/main/java/com/example/myapplication/User.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    var id:Int,
+    var firstName:String,
+    var lastName:String,
+    var age:Int
+)
